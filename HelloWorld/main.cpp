c07f40e40defad7ed426254529cee76507c15b02
stmscavengerhunt <49766010+stmscavengerhunt@users.noreply.github.com>
2020-03-25 11:05:25 -0400

    First commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Thin Thazin on 3/25/20.
+//  Copyright © 2020 Thin Thazin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    std::cout << "Hello, World!\n";
+    return 0;
+}
